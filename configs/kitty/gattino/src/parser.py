2cb08b1794e030bb76a539467b20b41303204fd9
snowyfloke <yasmin@snowfloke.com>
2026-04-20 01:03:44 -0300

    Updated dotfiles (2026-04-20)

--- configs/kitty/gattino/src/parser.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def extract_first_code_block(text: str) -> str:
-    text = text.replace('```bash', '```')
-    match = re.search(r"```(.*?)```", text, re.DOTALL)
-    if match:
-        return match.group(1).strip()
-    return ""
--- configs/nvim/nvim/lua/snowy-theme/colors/snowy-theme.lua
+++
@@ -1,1 +0,0 @@
-require("snowy-theme").setup()
