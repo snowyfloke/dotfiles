066f9ab86de4b0b260602a846cf6b82af89fae91
snowyfloke <yasmin@snowfloke.com>
2026-04-20 00:27:18 -0300

    removed duplicated scripts

--- scripts/scripts/starship.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-temp_file=$(mktemp)
-
-cat "/home/yasmin/.config/starship-preset.toml" >"$temp_file"
-
-cat "/home/yasmin/.cache/noctalia/starship-palette.toml" >>"$temp_file"
-
-mv "$temp_file" "/home/yasmin/.config/starship.toml"
